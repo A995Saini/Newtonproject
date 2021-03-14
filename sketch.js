aa61b03b515466d65ad06dc2f50a394d2b7a0404
irinsajan <69659076+irinsajan@users.noreply.github.com>
2021-03-14 18:13:58 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,6 +3,7 @@ const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
+const Constraint = Matter.Constraint;
 var ball1,ball2,ball3,ball4,ball5
 
 function preload()
@@ -14,16 +15,25 @@ function preload()
 
 function setup() {
 	createCanvas(800, 700);
-	ball1=new Ball(100,400,20)
-	ball2=new Ball(100,400,20)
-	ball3=new Ball(100,400,20)
-	ball4=new Ball(100,400,20)
-	ball5=new Ball(100,400,20)
 
 	engine = Engine.create();
 	world = engine.world;
 
-	//Create the Bodies Here.
+	ball1=new Ball(200,400,20)
+	ball2=new Ball(250,400,20)
+	ball3=new Ball(300,400,20)
+	ball4=new Ball(350,400,20)
+	ball5=new Ball(400,400,20)
+
+	roof = new Roof(300,100,400,10)
+
+	
+	rope1=new Rope(ball1.body,roof.body,-40*2,0)
+	rope2=new Rope(ball2.body,roof.body,-40,0)
+	rope3=new Rope(ball3.body,roof.body,0,0)
+	rope4=new Rope(ball4.body,roof.body,40,0)
+	rope5=new Rope(ball5.body,roof.body,40*2,0)
+
 
 
 	Engine.run(engine);
@@ -39,6 +49,14 @@ function draw() {
   ball3.display();
   ball4.display();
   ball5.display();
+
+  roof.display();
+
+  rope1.display();
+  rope2.display();
+  rope3.display();
+  rope4.display();
+  rope5.display();
   
   drawSprites();
  
